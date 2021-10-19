52a906717f7cabcd61452ec4c4950b7c5357dbc5
su <su@209-6>
2021-10-19 13:12:54 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a, b, c;
+                Console.WriteLine("Введите a");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите c");
+                c = double.Parse(Console.ReadLine());
+
+                double d = b * b - 4 * a * c;
+
+                if (d >= 0)
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine("x1 = " + x1);
+                    Console.WriteLine("x2 = " + x2);
+                }
+                else
+                {
+                    Console.WriteLine(" Уравнение не имеет корней");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
